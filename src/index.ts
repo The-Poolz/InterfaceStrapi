5c5c62b42ff63db1b292a7735b11b9c1abc427a1
DVitaliy <dynnik.vitaliy@gmail.com>
2025-04-02 12:56:07 +0300

    Issue #2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Provider from "./StrapiProvider"
 
 export * from "./endpoints/useFooter"
+export * from "./endpoints/useHeader"
 export * as types from "./__generated__/graphql"
 export * from "@apollo/client"
 export * from "@apollo/client/testing"
