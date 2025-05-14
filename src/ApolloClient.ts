41c78f16c81bb4a8b555c6affe9c8e80aafd9c44
DVitaliy <dynnik.vitaliy@gmail.com>
2025-05-14 14:16:35 +0300

    Issue #60

--- src/ApolloClient.ts
+++ src/ApolloClient.ts
@@ -1,6 +1,22 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client"
+import { CachePersistor, LocalStorageWrapper } from "apollo3-cache-persist"
+
+const cache = new InMemoryCache()
+export const newPersistor = new CachePersistor({
+  cache,
+  storage: new LocalStorageWrapper(window.localStorage),
+  debug: !!~location.hash.indexOf("debug")
+  // trigger: 'write',
+})
+await newPersistor.restore()
+
+console.log("cache restored")
 
 export const AClient = new ApolloClient({
   uri: "https://data.poolz.finance/graphql",
-  cache: new InMemoryCache()
+  cache,
+  defaultOptions: {
+    watchQuery: { fetchPolicy: "cache-first" },
+    query: { fetchPolicy: "cache-first" }
+  }
 })
--- src/endpoints/useFooter.ts
+++ src/endpoints/useFooter.ts
@@ -1,7 +1,9 @@
 import { useQuery, AClient } from "../index"
+import { useCacheWithUpdatedAt } from "../useCacheWithUpdatedAt"
 import { graphql } from "../__generated__"
+import * as types from "../__generated__/graphql"
 
-export const GET_FOOTER = graphql(`
+const GET_FOOTER = graphql(`
   query Footer {
     footer {
       Company {
@@ -34,8 +36,23 @@ export const GET_FOOTER = graphql(`
         url
         newWindow
       }
+      updatedAt
+    }
+  }
+`)
+
+const GET_FOOTER_UPDATED = graphql(`
+  query FooterUpdated {
+    footer {
+      updatedAt
     }
   }
 `)
 
 export const useFooter = () => useQuery(GET_FOOTER, { client: AClient, fetchPolicy: "cache-first" })
+export const useFooter_persist = () =>
+  useCacheWithUpdatedAt<NonNullable<types.FooterQuery>>({
+    fullQuery: GET_FOOTER,
+    updatedAtQuery: GET_FOOTER_UPDATED,
+    getUpdatedAt: (data) => data.footer?.updatedAt ?? Symbol()
+  })
