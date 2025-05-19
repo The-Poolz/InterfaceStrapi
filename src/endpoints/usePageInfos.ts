00210fc19d4ab6434d9305dcbb887f6d6c5efc35
DVitaliy <dynnik.vitaliy@gmail.com>
2025-05-19 12:53:55 +0300

    Issue #72

--- src/endpoints/usePageInfos.ts
+++ src/endpoints/usePageInfos.ts
@@ -1,18 +1,30 @@
-import { useQuery } from "../index"
+import { useCacheWithUpdatedAt } from "../useCacheWithUpdatedAt"
+import * as types from "../__generated__/graphql"
 import { graphql } from "../__generated__"
-import { useGetClient } from "../globalState/Context"
 
 const GET_PAGE_INFOS = graphql(`
   query PageInfos {
-    pageInfos {
+    pageInfos(sort: ["updatedAt:asc"]) {
       Link
       Title
       Description
+      updatedAt
+    }
+  }
+`)
+const GET_PAGE_INFOS_UPDATED = graphql(`
+  query PageInfosUpdated {
+    pageInfos(sort: ["updatedAt:asc"]) {
+      updatedAt
     }
   }
 `)
 
-export const usePageInfos = () => {
-  const AClient = useGetClient()
-  return useQuery(GET_PAGE_INFOS, { client: AClient })
-}
+export const usePageInfos = () =>
+  useCacheWithUpdatedAt<NonNullable<types.PageInfosQuery>, NonNullable<types.PageInfosUpdatedQuery>>({
+    fullQuery: GET_PAGE_INFOS,
+    updatedAtQuery: GET_PAGE_INFOS_UPDATED,
+    getUpdatedAt: (data) => {
+      return data.pageInfos?.map((c) => c?.updatedAt ?? "").join(",") ?? ""
+    }
+  })
