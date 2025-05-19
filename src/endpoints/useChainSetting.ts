f970a30d9e7bc80f64c0849a21d7c0ffe82fe8e7
DVitaliy <dynnik.vitaliy@gmail.com>
2025-05-19 12:14:35 +0300

    Issue #72

--- src/endpoints/useChainSetting.ts
+++ src/endpoints/useChainSetting.ts
@@ -1,6 +1,6 @@
-import { useQuery } from "../index"
+import { useCacheWithUpdatedAt } from "../useCacheWithUpdatedAt"
+import * as types from "../__generated__/graphql"
 import { graphql } from "../__generated__"
-import { useGetClient } from "../globalState/Context"
 
 const GET_CHAINS_OF_PROJECTS = graphql(`
   query Chain_setting {
@@ -10,13 +10,23 @@ const GET_CHAINS_OF_PROJECTS = graphql(`
         chainId
         name
       }
+      updatedAt
     }
   }
 `)
 
-export const useChainSetting = () => {
-  const AClient = useGetClient()
-  return useQuery(GET_CHAINS_OF_PROJECTS, {
-    client: AClient
+const GET_CHAINS_OF_PROJECTS_UPDATED = graphql(`
+  query Chain_settingUpdated {
+    chainSettings(filters: { projects_informations: { IsShow: { eq: true } } }) {
+      updatedAt
+    }
+  }
+`)
+export const useChainSetting = () =>
+  useCacheWithUpdatedAt<NonNullable<types.Chain_SettingQuery>, NonNullable<types.Chain_SettingUpdatedQuery>>({
+    fullQuery: GET_CHAINS_OF_PROJECTS,
+    updatedAtQuery: GET_CHAINS_OF_PROJECTS_UPDATED,
+    getUpdatedAt: (data) => {
+      return data.chainSettings?.map((c) => c?.updatedAt ?? "").join(",") ?? ""
+    }
   })
-}
