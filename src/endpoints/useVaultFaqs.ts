df0358390f2f6137b5e1095d486889a8084b5f05
DVitaliy <dynnik.vitaliy@gmail.com>
2025-05-19 14:35:05 +0300

    Issue #72

--- src/endpoints/useVaultFaqs.ts
+++ src/endpoints/useVaultFaqs.ts
@@ -1,17 +1,29 @@
-import { useQuery } from "../index"
+import { useCacheWithUpdatedAt } from "../useCacheWithUpdatedAt"
+import * as types from "../__generated__/graphql"
 import { graphql } from "../__generated__"
-import { useGetClient } from "../globalState/Context"
 
 const GET_VAULT_FAGS = graphql(`
   query VaultFaqs {
     vaultFaqs {
       ShortText
       LongText
+      updatedAt
+    }
+  }
+`)
+const GET_VAULT_FAGS_UPDATED = graphql(`
+  query VaultFaqsUpdated {
+    vaultFaqs {
+      updatedAt
     }
   }
 `)
 
-export const useVaultFaqs = () => {
-  const AClient = useGetClient()
-  return useQuery(GET_VAULT_FAGS, { client: AClient })
-}
+export const useVaultFaqs = () =>
+  useCacheWithUpdatedAt<NonNullable<types.VaultFaqsQuery>, NonNullable<types.VaultFaqsUpdatedQuery>>({
+    fullQuery: GET_VAULT_FAGS,
+    updatedAtQuery: GET_VAULT_FAGS_UPDATED,
+    getUpdatedAt: (data) => {
+      return data.vaultFaqs?.map((c) => c?.updatedAt ?? "").join(",") ?? ""
+    }
+  })
--- src/useCacheWithUpdatedAt.ts
+++ src/useCacheWithUpdatedAt.ts
@@ -2,13 +2,19 @@ import { useEffect, useRef } from "react"
 import { useQuery, useLazyQuery, type DocumentNode, NetworkStatus } from "./index"
 import { useGetClient } from "./globalState/Context"
 
-interface IHookProps<TFull, TUpdated> {
+interface IHookProps<TFull, TUpdated, TVariables extends Record<string, unknown> | undefined = undefined> {
   fullQuery: DocumentNode
   updatedAtQuery: DocumentNode
   getUpdatedAt: (data: TUpdated | TFull) => string
+  variables?: TVariables
 }
 
-export const useCacheWithUpdatedAt = <TFull, TUpdated>({ fullQuery, updatedAtQuery, getUpdatedAt }: IHookProps<TFull, TUpdated>) => {
+export const useCacheWithUpdatedAt = <TFull, TUpdated, TVariables extends Record<string, unknown> | undefined = undefined>({
+  fullQuery,
+  updatedAtQuery,
+  getUpdatedAt,
+  variables
+}: IHookProps<TFull, TUpdated, TVariables>) => {
   const client = useGetClient()
 
   const {
@@ -19,6 +25,7 @@ export const useCacheWithUpdatedAt = <TFull, TUpdated>({ fullQuery, updatedAtQue
     networkStatus
   } = useQuery<TFull>(fullQuery, {
     client,
+    variables,
     fetchPolicy: "cache-first"
   })
   const isFromCache = useRef(networkStatus === NetworkStatus.ready && !fullLoading).current
@@ -30,9 +37,9 @@ export const useCacheWithUpdatedAt = <TFull, TUpdated>({ fullQuery, updatedAtQue
 
   useEffect(() => {
     if (isFromCache) {
-      getLazy()
+      getLazy({ variables })
     }
-  }, [isFromCache, getLazy])
+  }, [isFromCache, variables, getLazy])
 
   useEffect(() => {
     if (!updatedLoading && updatedData && fullData) {
